d3ca9dc5342300b050ce68474cf346238a75c2a8
shakiburcmt <shakibur.cmt@gmail.com>
2022-10-14 00:13:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const phones=require('./phones.json')
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World from Node');
+})
+
+app.get('/phones', (req, res) => {
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log('looking for data for id:', id)
+
+    const phone = phones.find(ph => ph.id === id) || {};
+    res.send(phone)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+})
